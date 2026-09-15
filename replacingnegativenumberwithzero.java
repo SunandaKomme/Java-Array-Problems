93d238e679c64071843f56613568dfe29efc834e
SunandaKomme <kommesunanda@gmail.com>
2026-09-15 11:31:57 +0530

    Added replacingnegativenumberwithzero

---
+++ replacingnegativenumberwithzero.java
@@ -0,0 +1,21 @@
+//. Replace every negative number with 0.
+import java.util.*;
+public class replacingnegativenumberwithzero{
+    public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int[]arr=new int[n];
+    for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    for(int i=0;i<n;i++){
+        if(arr[i]<0){
+            arr[i]=0;
+            
+        }
+    }
+     
+System.out.println(Arrays.toString(arr));
+}
+}
+

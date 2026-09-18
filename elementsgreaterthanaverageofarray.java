7d61ff80412bf232a429ea96daf6f51874cde7c6
SunandaKomme <kommesunanda@gmail.com>
2026-09-18 23:20:05 +0530

    elementsgreaterthanaverageofarray

---
+++ elementsgreaterthanaverageofarray.java
@@ -0,0 +1,24 @@
+// Count how many elements are greater than the average of the array. 
+import java.util.*;
+public class elementsgreaterthanaverageofarray{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[]arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+        int avg=sum/n;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]>avg){
+                count++;
+            }
+        }
+        System.out.print(count);
+
+    }}
